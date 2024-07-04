a108d01ce7e8b0ede7abcbdf25968495b621f54d
Thibaut <rack.kendorim@gmail.com>
2024-07-04 17:08:44 +0200

    :tada: Minimal Project

---
+++ src/main/java/com/kuro/hr_webapp/HrWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.kuro.hr_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuro/hr_webapp/HrWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuro.hr_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
